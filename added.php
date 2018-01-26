0e52de5c8eae9929cbb02bf08d05488ee5556a66
FTam <ftam@workanyware.co.uk>
2018-01-26 10:01:47 +0000

    catalogue

---
+++ added.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: FTam
+ * Date: 26/01/2018
+ * Time: 10:01
+ */
--- shop.php
+++ shop.php
@@ -12,7 +12,40 @@ if (!isset($_SESSION['member_id'])) {
     require('login_tools.php');
     load();
 }
+
+$mysqli = new mysqli("localhost", "root", "", "FirstProject");
+
+$result = $mysqli->query("SELECT * FROM Items");
+
 ?>
-SHOP
+<div class="container">
+    <h3><strong>THE SHOP</strong></h3>
+    <table class="table table-hover">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Price</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+                <?php
+                if($result->num_rows > 0) {
+                    while($row = $result->fetch_array(MYSQLI_ASSOC)) {
+                        echo '<tr>';
+                        echo '<td>' . $row['item_name'] . '</td>';
+                        echo '<td>' . $row['item_desc'] . '</td>';
+                        echo '<td>£' . $row['item_price'] . '</td>';
+                        echo '<td><button type="button" class="btn btn-info"><a href="added.php?id=' . $row['item_id'] . '">Add to Cart</a></button></td>';
+                        echo '</tr>';
+                    }
+                }
+                $mysqli->close();
+                ?>
+        </tbody>
+    </table>
+</div>
+
 </body>
 </html>
